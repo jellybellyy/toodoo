ba6c2ff0a9fcc0737d66dea4fada82e9ec5410b3
joey <joeywongsw@gmail.com>
2020-09-07 13:29:56 +0800

    Edited db.js

--- db.js
+++ db.js
@@ -18,8 +18,10 @@ if( process.env.DATABASE_URL ){
     password: auth[1],
     host: params.hostname,
     port: params.port,
-    database: params.pathname.split('/')[1],
-    ssl: true
+    database: params.pathname.split("/")[1],
+    ssl: {
+      rejectUnauthorized: false,
+    },
   };
 
 }else{
